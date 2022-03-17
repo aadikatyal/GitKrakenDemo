d389b827ec237df84e48f617384bcb21cc64baaa
Anurag <anurag.tilwe6@gmail.com>
2022-03-16 23:06:04 -0400

    Runnner and interface for printing quotient functionality created

--- Runner.java
+++ Runner.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
 public class Runner
 {
 	public static void main(String[] args)
 	{
-		
+
+		Scanner in = new Scanner(System.in);
+
+		System.out.print("Enter a dividend number: ");
+		float d1 = Float.valueOf(in.nextLine());
+
+		System.out.print("Enter the divisor: ");
+		float d2 = Float.valueOf(in.nextLine());
+
+		Quotient q = (f1, f2) -> {
+			return (f1/f2);
+		};
+
+		System.out.printf("%1.3f%n", q.divide(d1, d2));
+
 	}
 }
